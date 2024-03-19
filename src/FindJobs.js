3fb5dc6b27797182cbda03ff659c247f030fbf0a
Deborah Kudaru <deborahkudaru@gmail.com>
2024-03-19 02:12:29 +0100

    chnages

---
+++ src/FindJobs.js
@@ -0,0 +1,49 @@
+const findJobs = [
+  {
+    nameOfJob: "",
+    logo: "",
+    location: "",
+    duration: "",
+    time: "",
+    company: ""
+  },
+
+  {
+    nameOfJob: "",
+    logo: "",
+    location: "",
+    duration: "",
+    time: "",
+    company: ""
+  },
+
+  {
+    nameOfJob: "",
+    logo: "",
+    location: "",
+    duration: "",
+    time: "",
+    company: ""
+  },
+
+  {
+    nameOfJob: "",
+    logo: "",
+    location: "",
+    duration: "",
+    time: "",
+    company: ""
+  },
+
+  {
+    nameOfJob: "",
+    logo: "",
+    location: "",
+    duration: "",
+    time: "",
+    company: ""
+  }
+
+]
+
+export default findJobs;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <div className='bg-zinc-200 mx-auto flex w-full  items-center justify-between flex-wrap z-20 shadow-md p-4 '>
+    <div className='bg-zinc-200 mx-auto flex w-full  items-center justify-between flex-wrap z-20 shadow-md p-4 fixed top-100'>
       <Logo />
       <Navbar />
     </div>
