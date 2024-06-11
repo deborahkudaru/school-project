4122a63edb4e9d566f1e4abf7730f2a91fac848b
Deborah Kudaru <deborahkudaru@gmail.com>
2024-06-11 17:52:10 +0100

    issues

--- src/pages/Workersignup.jsx
+++ src/pages/Workersignup.jsx
@@ -6,41 +6,60 @@ import { FaGoogle } from "react-icons/fa";
 
 const Workersignup = () => {
   return (
-    <div className="px-48 pt-16">
-      <Logo />
-      <p>Looking to hire</p>
-      <Link>Apply as employer</Link>
-      <div className="flex flex-col gap-3 mt-5">
-        <h2 className="text-2xl font-semibold">Sign up to hire</h2>
+    <div className="px-48 pt-10">
+    <Logo />
+    <div className="flex flex-row gap-2 justify-end">
+      <p>Looking to hire?</p>
+      <Link to="/employer-signup" className="text-violet-400">
+        Apply as an employer
+      </Link>
+    </div>
+    <div className=" mt-5 w-1/2 m-auto text-center">
+      <h2 className="text-2xl font-semibold">Sign up to apply</h2>
+      <div className="flex gap-3">
         <button className="bg-blue-500 text-gray-50 font-semibold w-2/3 m-auto pb-3 rounded-lg">
-          <FaGoogle className="relative top-5 left-14 text-rose-500" />
+          <FaGoogle className="relative top-5 left-5 text-rose-500" />
           Continue with Google
         </button>
-        <button className="border-2 border-black w-2/3 m-auto font-semibold pb-3 rounded-lg mt-2">
-          <FaApple className="relative top-5 left-14" />
+        <button className="border-2 border-black w-2/3 m-auto font-semibold pb-2 rounded-lg mt-2">
+          <FaApple className="relative top-5 left-7" />
           Continue with Apple
         </button>
-        <p>or</p>
-        <label htmlFor="">First Name</label>
-        <input type="text" />
-        <label htmlFor="">Last Name</label>
-        <input type="text" />
-        <label htmlFor="email">Work Email Address</label>
-        <input type="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" />
-        <button>Create my account</button>
-        <p>
-          Already have an account?{" "}
-          <Link
-            to="login"
-            className="text-violet-600 underline underline-offset-1"
-          >
-            Log in
-          </Link>
-        </p>
       </div>
+      <p>or</p>
+      <div className="flex gapa-2">
+        <div className="flex flex-col">
+          <label htmlFor="">First Name</label>
+          <input type="text" />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="">Last Name</label>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col">
+          <label htmlFor="email">Work Email Address</label>
+          <input type="email" />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password">Password</label>
+          <input type="password" />
+        </div>
+      </div>
+
+      <button className="block">Create my account</button>
+      <p>
+        Already have an account?{" "}
+        <Link
+          to="/login"
+          className="text-violet-600 underline underline-offset-1"
+        >
+          Log in
+        </Link>
+      </p>
     </div>
+  </div>
   );
 };
 
